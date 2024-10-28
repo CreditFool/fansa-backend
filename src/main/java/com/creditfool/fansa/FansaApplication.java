b30535c14a4be7d78f2e51de3a8a9bd75722f618
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-10-28 13:17:34 +0700

    project init

---
+++ src/main/java/com/creditfool/fansa/FansaApplication.java
@@ -0,0 +1,13 @@
+package com.creditfool.fansa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FansaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FansaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creditfool/fansa/FansaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditfool.fansa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FansaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
